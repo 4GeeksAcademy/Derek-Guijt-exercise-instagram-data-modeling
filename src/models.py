d7769d2f1c13188938b6ba3437cd4b9e74d0462d
Derek Guijt <137216062+DerekGuijt@users.noreply.github.com>
2024-02-15 23:38:04 +0000

    done

--- src/models.py
+++ src/models.py
@@ -7,26 +7,43 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table user
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    posts = relationship("Post", backref = "user")
+    followers = relationship("Follower", backref = "user")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    post_text = Column(String(250), nullable=False)
+    user = Column(Integer, ForeignKey("user.id"))
+    comments = relationship("Comment", backref = "post")
+    reactions = relationship("Reaction", backref = "post")
 
-    def to_dict(self):
-        return {}
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False)
+    post = Column(Integer, ForeignKey("post.id"))
+    
+
+class Reaction(Base):
+    __tablename__ = 'reaction'
+    id = Column(Integer, primary_key=True)
+    reaction_type = Column(String(250), nullable=False)
+    post = Column(Integer, ForeignKey("post.id"))
+
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(250), nullable=False)
+    following = Column(Integer, ForeignKey("user.id"))
+
 
 ## Draw from SQLAlchemy base
 try:
